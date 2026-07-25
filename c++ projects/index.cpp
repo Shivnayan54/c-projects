485418b54c432bf85a31d86ac5818d2c450551e1
shivnayan <nayan54451@gmail.com>
2026-07-25 20:33:54 +0530

    working on tic tac toe

--- c++ projects/index.cpp
+++ c++ projects/index.cpp
@@ -14,6 +14,20 @@ void drawBoard()
     
 }
 
+bool placemarker(int slot){
+    int row = (slot-1)/3; 
+    int col=  (slot-1)%3; 
+
+    if(board[row][col] != 'x' && board[row][col]!= '0')
+    {
+        board[row][col] = currentMarker; 
+        return true; 
+    }
+    else{
+        return false; 
+    }
+}
+
 void game()
 {
     cout<<"Player choose your marker: x or 0"; 
@@ -23,6 +37,27 @@ void game()
     currentPlayer=1; 
     currentMarker = markerP1; 
     drawBoard(); 
+    int playerwon;
+    for(int i=0; i<9 ; i++)
+     {
+        cout<<"its player"<<currentPlayer<<" S turn Enter your slot:"; 
+        int slot; 
+        cin>>slot; 
+
+        if(slot<1 || slot> 9 ){
+            cout<<"it is not a valid slot , you may try again"; 
+            i--; 
+            continue; 
+        }
+        if(!placemarker(slot)){ 
+            cout<<"slot occupied! try again \n"; 
+            i--; 
+            continue; 
+        }
+        drawBoard(); 
+        playerwon=winner(); 
+
+     }
 }
 
 
